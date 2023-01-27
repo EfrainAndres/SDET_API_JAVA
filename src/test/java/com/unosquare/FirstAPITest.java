913ae030e8dbd521e2c64c05ec9c1bb2a3f4dc4b
Efrain Andres Vergara Serrato <efrainvergara.udec@gmail.com>
2023-01-26 22:18:35 -0500

    Create a POST Request using JSON Files

--- src/test/java/com/unosquare/FirstAPITest.java
+++ src/test/java/com/unosquare/FirstAPITest.java
@@ -12,6 +12,11 @@ import org.testng.Reporter;
 import org.testng.annotations.BeforeMethod;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import org.json.JSONObject;
+
 import static io.restassured.RestAssured.*;
 
 public class FirstAPITest {
@@ -33,15 +38,54 @@ public class FirstAPITest {
   
   @Test
   public void f_Gherkin() {
-	  
 	  given()
 	  .when()
 	  	.get("https://reqres.in/api/users/2")
-	  		.then().assertThat().statusCode(200).assertThat().contentType(ContentType.JSON);
+	  		.then()
+	  		.assertThat().statusCode(200)
+	  		.assertThat().contentType(ContentType.JSON);
 	  
-	  Reporter.log("Sucess 200 validation");
+	  Reporter.log("Success 200 validation");
   }
-
+  
+  @Test
+  public void PostCreate() {
+	  RestAssured.baseURI = "https://reqres.in/";
+      JSONObject requestParams = new JSONObject();
+      requestParams.put("name", "John");
+      requestParams.put("job", "QA Engineer");
+      
+      Response response = RestAssured.given()
+              .header("Content-Type", "application/json")
+              .body(requestParams.toString())
+              .post("/api/users");
+      
+      int statusCode = response.getStatusCode();
+      String body = response.getBody().asString();
+      
+      Reporter.log("Response Code: " + statusCode);
+      Reporter.log("Response Body: " + body);
+      Reporter.log("JSON Body: " + requestParams);
+  }
+  
+  @Test
+  public void PostRegister() throws FileNotFoundException {
+	  RestAssured.baseURI = "https://reqres.in/";
+	  FileReader jsonFile = new FileReader("src/test/resources/Json/Register.json");
+      JSONObject requestParams = new JSONObject(jsonFile);
+      
+      Response response = RestAssured.given()
+              .header("Content-Type", "application/json")
+              .body(requestParams.toString())
+              .post("/api/users");
+      
+      int statusCode = response.getStatusCode();
+      String body = response.getBody().asString();
+      
+      Reporter.log("Response Code: " + statusCode);
+      Reporter.log("Response Body: " + body);
+      Reporter.log("JSON Body: " + requestParams);
+  }
   @BeforeMethod
   public void beforeMethod() {
   }
